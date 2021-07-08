e21db298ee264cf83b2b77c58edca6e73d8ccc99
Mathijs Vogelzang <mathijs@gmail.com>
2021-07-08 22:06:58 +0200

    Make paraswap work on mainnet (convert transaction from web3.js format to ethers.js)

--- web/src/web3/paraswapService.ts
+++ web/src/web3/paraswapService.ts
@@ -1,7 +1,7 @@
 import { PlannedOrder } from '@/orderplan/orderplan';
 import { fixedToBigNumber } from '@/util/numbers';
-import { BigNumber } from 'ethers';
-import { OptimalRatesWithPartnerFees, ParaSwap, UNLIMITED_ALLOWANCE } from 'paraswap';
+import { BigNumber, ethers } from 'ethers';
+import { OptimalRatesWithPartnerFees, ParaSwap, UNLIMITED_ALLOWANCE, Transaction } from 'paraswap';
 import { TransactionResult } from './uniswapService';
 import { web3Service } from './web3Service';
 
@@ -18,12 +18,18 @@ interface ParaSwapPlan {
 export type ParaSwapPredictedOutput = PredictedOutput & ParaSwapPlan;
 
 class ParaswapService {
+  private referrer = 'TokenBlend';
+
   public async getPredictedOutput(order: PlannedOrder): Promise<ParaSwapPredictedOutput> {
     paraSwap.setWeb3Provider(web3Service.getExternalProvider());
     const priceRoute = await paraSwap.getRate(
       order.fromToken.id,
       order.toToken.id,
-      fixedToBigNumber(order.sendAmount, order.fromToken.decimals).toString()
+      fixedToBigNumber(order.sendAmount, order.fromToken.decimals).toString(),
+      undefined,
+      {
+        referrer: this.referrer,
+      }
     );
     console.log(priceRoute);
     if ('message' in priceRoute) {
@@ -60,6 +66,12 @@ class ParaswapService {
         order.fromToken.id
       );
       console.log(`allowance result: ${allowanceResult}`);
+      if (allowanceResult) {
+        // This is a transaction id, wait for it to complete.
+        const tx = await web3Service.getProvider().getTransaction(allowanceResult);
+        const receipt = await tx.wait();
+        console.log('allowance tx receipt', receipt);
+      }
     }
 
     const tx = await paraSwap.buildTx(
@@ -69,18 +81,49 @@ class ParaswapService {
       plan.predictedOutput.toString(),
       plan.priceRoute,
       address,
-      'trading-bot'
+      this.referrer,
+      undefined,
+      undefined,
+      order.fromToken.decimals,
+      order.toToken.decimals
     );
     if ('message' in tx) {
       throw new Error('error building tx: ' + tx.message);
     }
-    const sentTx = await web3Service.getSigner().sendTransaction(tx);
+
+    console.log('paraswap transaction', tx);
+    const ethersTx = this.convertWeb3ToEthers(tx);
+
+    console.log('Converted to ethers', ethersTx);
+    const sentTx = await web3Service.getSigner().sendTransaction(ethersTx);
     const receipt = await sentTx.wait();
     console.log(receipt);
 
     return {
       message: 'Success',
       success: true,
+    };
+  }
+
+  private convertWeb3ToEthers(
+    tx: Transaction & { gas?: string; gasPrice?: string }
+  ): ethers.providers.TransactionRequest {
+    let gasLimit: BigNumber | undefined = undefined;
+    let gasPrice: BigNumber | undefined = undefined;
+    if ('gas' in tx) {
+      gasLimit = BigNumber.from(tx['gas']);
+    }
+    if ('gasPrice' in tx) {
+      gasPrice = BigNumber.from(tx['gasPrice']);
+    }
+    return {
+      chainId: tx.chainId,
+      from: tx.from,
+      to: tx.to,
+      value: BigNumber.from(tx.value),
+      data: tx.data,
+      gasPrice,
+      gasLimit,
     };
   }
 }
